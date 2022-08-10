5b3214cad26ee58d0c234d0eb0e36765ebc0f076
grebocheck <gadzilayer@gmail.com>
2022-08-10 21:31:45 +0300

    main.py while true

--- main_loc.py
+++ main_loc.py
@@ -1,7 +1,9 @@
 from aiogram import executor
 from bot.bote import *
 
-
 if __name__ == '__main__':
     while True:
-        executor.start_polling(dp, skip_updates=True)
+        try:
+            executor.start_polling(dp, skip_updates=True)
+        except Exception as ex:
+            print(ex)
