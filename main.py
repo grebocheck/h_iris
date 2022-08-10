453f07d63a1965d4f1eada84bd6d67dca831b5e5
grebocheck <gadzilayer@gmail.com>
2022-08-10 21:25:39 +0300

    main.py while true

--- main.py
+++ main.py
@@ -24,13 +24,14 @@ async def on_shutdown(dispatcher):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        skip_updates=True,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
+    while True:
+        logging.basicConfig(level=logging.INFO)
+        start_webhook(
+            dispatcher=dp,
+            webhook_path=WEBHOOK_PATH,
+            skip_updates=True,
+            on_startup=on_startup,
+            on_shutdown=on_shutdown,
+            host=WEBAPP_HOST,
+            port=WEBAPP_PORT,
+        )
--- main_loc.py
+++ main_loc.py
@@ -3,4 +3,5 @@ from bot.bote import *
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    while True:
+        executor.start_polling(dp, skip_updates=True)
